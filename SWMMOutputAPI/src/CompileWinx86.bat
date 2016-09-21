0a299d73395ecf448dc82ed7bd4576df72c0efd9
Bryant E. McDonnell <bemcdonnell@gmail.com>
2016-09-20 20:09:17 -0400

    rearanging and updates to setup file and DLL path

---
+++ SWMMOutputAPI/src/CompileWinx86.bat
@@ -0,0 +1,7 @@
+echo off
+set PATH=%PATH%;C:\MinGW\bin
+
+gcc.exe -c -Wall outputAPI.c datetime.c 
+gcc.exe -mwindows -o C:\PROJECTCODE\SWMMOutputAPI\SWMMOutputAPI\data\outputAPI_winx86.dll -shared outputAPI.o datetime.o
+
+del /S *.o
