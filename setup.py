8e4f473be355498e220f7b5dfd2ddc55fd758a2b
Bryant E. McDonnell <bemcdonnell@gmail.com>
2016-09-20 20:30:16 -0400

    Repairing Commit after stash pop mishap

--- setup.py
+++ setup.py
@@ -1,16 +1,30 @@
 
-#Python setup.py sdist
-
 from distutils.core import setup
 
-setup(name = 'SWMMOutputAPI',
-      version = '2.0',
-      description = 'Python Wrapper for SWMM Output API',
-      author = 'Bryant E. McDonnell',
-      author_email='bemcdonnell@gmail.com',
-      url = 'https://github.com/bemcdonnell/SWMMOutputAPI.git',
-      package_dir = {'SWMMOutputAPI':'SWMMOutputAPI'},
-      package_data = {'SWMMOutputAPI':\
-                 ['../*.py',
-                 'data/outputAPI_winx86.dll']}
-      )
+
+
+VERSION = '0.2.1'  
+AUTHOR_NAME = 'Bryant E. McDonnell (EmNet LLC)'
+AUTHOR_EMAIL = 'bemcdonnell@gmail.com'
+
+
+
+setup(name='SWMMOutputAPI',
+      version=VERSION,
+      description='Python Wrapper for SWMM5 Binary Output API',
+      author=AUTHOR_NAME,
+      url='https://github.com/bemcdonnell/SWMMOutputAPI.git',
+      author_email=AUTHOR_EMAIL,
+
+      package_dir = {'':'SWMMOutputAPI'},
+      packages=[''],
+      package_data = {'':
+                      ['src/*.c',\
+                       'src/*.h',\
+                       'data/outputAPI_winx86.dll']},
+      license="BSD2 License",
+      classifiers=[
+          "License :: OSI Approved :: BSD2 License",
+          "Programming Language :: Python :: 2.7",
+      ]
+)
--- src/CompileWinx86.bat
+++
@@ -1,7 +0,0 @@
-echo off
-set PATH=%PATH%;C:\MinGW\bin
-
-gcc.exe -c -Wall outputAPI.c datetime.c 
-gcc.exe -mwindows -o C:\PROJECTCODE\SWMMOutputAPI\data\outputAPI_winx86.dll -shared outputAPI.o datetime.o
-
-del /S *.o
